1c517ccbf2b9e107e50be64e93aff0e96336f22b
Mohammad Rasti <mohammad.rasti90@gmail.com>
2020-04-30 16:51:02 +0430

    some refactoring

--- Service/Contract/IHubService.cs
+++ Service/Contract/IHubService.cs
@@ -4,10 +4,10 @@ using ChatServer.Model.ViewModels;
 
 namespace ChatServer.Service.Contract {
     public interface IHubService {
-        Task<User> FindUserInChat (string appId, string chatId, string userId);
+        Task<bool> FindUserInChat (string appId, string chatId, string userId);
         Task<UserChatVM> GetChatHistoryAndDoAppropriateActions (string appId, string chatId, string userId);
         Task<ChatConversation> SendMessageToChat (string appId, string chatId, string userId, string message);
-        Task<User> MakeUserOnline (string appId, string userId, string connectionId, string access_token);
-        Task<User> MakeUserOffline (string appId, string userId, string connectionId, string access_token);
+        Task<bool> MakeUserOnline (string appId, string userId, string connectionId, string access_token);
+        Task<bool> MakeUserOffline (string appId, string userId, string connectionId, string access_token);
     }
 }
